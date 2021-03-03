73ff16694c5331a6e5fa1460b94bdbeeffc26d41
noavrd <noavrd@gmail.com>
2021-03-03 03:02:20 +0200

    update router

--- index.js
+++ index.js
@@ -1,26 +1,8 @@
 const app = require("./app");
 const PORT = process.env.PORT || 3000;
 
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
 
-class DatBase {
-  constructor (originalUrl) {
-    this.creationDate = dateSql();
-    this.redirectCount = 0;
-    this.originalUrl = originalUrl;
-    this.shorturlId = shorturlId();
-  }
-  
-}
-
-function dateSql() {
-  let date = new Date();
-  date = date.toISOString().split('T')[0] + " " + date.toTimeString().split(" ")[0];
-  return date;
-}
-
-function shorturlId() {
-  return Math.floor(Math.random() * 10000);
-}
