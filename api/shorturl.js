cc84f38f24f4dc2282b9b8065d9c125777f37db5
noavrd <noavrd@gmail.com>
2021-05-11 12:52:32 +0300

    update design

--- api/shorturl.js
+++ api/shorturl.js
@@ -11,13 +11,12 @@ router.post('/new', async (request, response) => {
   const url = request.body.url;
 
   if (!validUrl.isUri(url)) {
-    response
-      .status(400)
-      .send('Invalid URL. Please enter a valid url for shortening.');
+    response.render('error');
   } else {
     try {
       let shortUrl = await DataBase.addUrl(url);
-      response.status(200).send(`Your new URL: localhost:3000/${shortUrl}`);
+      response.render('displayUrl', { shortUrl });
+      // response.status(200).send(`Your new URL: localhost:3000/${shortUrl}`);
     } catch (e) {
       response.status(500).send('Internal Server Error!'`${e}`);
     }
